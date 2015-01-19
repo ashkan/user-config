26a2cbc6c93c684ad587b8d6719ff106a4c8f044
Aidin <aidin@Aidins-iMac.local>
2015-01-19 01:18:57 -0800

    Added slate. Reorganization. Added rudimentary support for cobalt postinstall. Added autocommands for plugins.json that runs cobalt.

---
+++ slate/slate.js
@@ -0,0 +1,12 @@
+
+var hyper = "ctrl;shift;alt;cmd";
+
+function generateActions(gridWidth, gridHeight) {
+  // var array = [];
+  // for 
+  //   var array = options
+}
+
+slate.bind("1:" + hyper, function(win) {
+  win.doOperation();
+});
