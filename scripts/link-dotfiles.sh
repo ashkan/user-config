362194f6bbd8fb40b4c07ee364e3e2fcf7c5ad4d
Ashkan Kiani <ashkan.k.kiani@gmail.com>
2015-07-23 14:28:54 -0700

    Added slate linking.

--- scripts/link-dotfiles.sh
+++ scripts/link-dotfiles.sh
@@ -7,10 +7,13 @@ realpath() {
 
 BASE_DIR=$(dirname "$0" && cd .. && pwd)
 RESOURCE_DIR="$BASE_DIR/scripts/resources/"
+DOTFILES_PATH=$(realpath "$SCRIPT_DIR/../dotfiles")
 
-ln -sf $(realpath "$SCRIPT_DIR/../dotfiles/vimrc") ~/.vimrc
-ln -sf $(realpath "$SCRIPT_DIR/../dotfiles/vim") ~/.vim
+ln -sf "$DOTFILES_PATH/vimrc" ~/.vimrc
+ln -sf "$DOTFILES_PATH/vim" ~/.vim
 
-ln -sf $(realpath "$SCRIPT_DIR/../dotfiles/vimrc") ~/.nvimrc
-ln -sf $(realpath "$SCRIPT_DIR/../dotfiles/vim") ~/.nvim
+ln -sf "$DOTFILES_PATH/vimrc" ~/.nvimrc
+ln -sf "$DOTFILES_PATH/vim" ~/.nvim
+
+ln -sf "$DOTFILES_PATH/slate/slate" ~/.slate
 
