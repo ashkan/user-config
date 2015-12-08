6b125c5eb37f3e16505137ed30131885d1db8c1e
Ashkan Kiani <ashkan.k.kiani@gmail.com>
2015-12-07 16:50:29 -0800

    Switches to zgen. Updates vimrc to be lazier. Uses deoplete instead of ycm.

---
+++ dotfiles/vim/init.vim
@@ -0,0 +1,1 @@
+/Users/ashkan/.vimrc
---
+++ dotfiles/zsh-config.zsh
@@ -0,0 +1,92 @@
+# vim:fdm=syntax
+# Customize to your needs...
+dash() { open "dash://$(echo "$*" | tr -d ' ')"; }
+
+# vim/editor config {{{
+alias vim=nvim
+alias :e=vim
+export EDITOR=nvim
+export VISUAL=nvim
+# }}}
+
+test -n "$NVIM_LISTEN_ADDRESS" && {
+  alias nvim="nvas"
+}
+
+# Go variables {{{
+export GOPATH=$HOME/Workspace/golang
+export GOBIN=$GOPATH/bin
+path+=( $GOBIN )
+# }}}
+
+# Zsh settings config {{{
+unsetopt CORRECT
+setopt CLOBBER
+unsetopt HIST_VERIFY
+setopt autocd
+# }}}
+
+# TODO.txt {{{
+export TODOTXT_DEFAULT_ACTION=ls
+alias t='todo.sh -d ~/.todo/config'
+# }}}
+
+# Path extras {{{
+path+=( $HOME/Workspace/bin )
+test -e /opt/local/bin && path+=/opt/local/bin
+# }}}
+
+alias ls="ls -G"
+
+function chpwd() {
+  emulate -L zsh
+  ls
+  # dirn=${PWD##*/}
+  # test -e "$WORKON_HOME/$dirn" && workon "$dirn"
+  test -f .zsh && source .zsh
+}
+
+alias http-serve='python3 -m http.server'
+alias rm=rmtrash
+alias l1="ls -1"
+# export COLORTERM=xterm-256color
+
+# Megashares client
+msc() {
+  curl -C - -O "$(megashares-client "$@")"
+}
+
+# multirust {{{
+export PATH="/Users/ashkan/.multirust/bin:$PATH"
+export PATH="/Users/ashkan/.multirust/cargo/bin:$PATH"
+# }}}
+
+# (mk)virtualenv {{{
+# source /usr/local/bin/virtualenvwrapper_lazy.sh
+workon() {
+  source $WORKON_HOME/$1/bin/activate
+}
+
+mkv() {
+  test -z "$1" && {
+    echo "Usage: mkv <directory>"
+    return
+  }
+  mkvirtualenv "$1"
+  cat <<EOF > ~/.virtualenvs/$1/bin/postactivate
+#!/usr/bin/env zsh
+cd $PWD/$1
+EOF
+}
+
+# export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx
+
+export WORKON_HOME=$HOME/.virtualenvs
+# }}}
+
+# # Base16 Shell
+# BASE16_SCHEME="default"
+# BASE16_SHELL="$HOME/.config/base16-shell/base16-$BASE16_SCHEME.dark.sh"
+# [[ -s $BASE16_SHELL ]] && . $BASE16_SHELL
+
+path=( $^path(N) )
