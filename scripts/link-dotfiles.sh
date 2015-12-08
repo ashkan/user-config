465036f1c6f320474b8c428ae309a29b0031b3ab
Ashkan Kiani <ashkan.k.kiani@gmail.com>
2015-12-07 18:27:44 -0800

    Improves link-dotfiles.

--- scripts/link-dotfiles.sh
+++ scripts/link-dotfiles.sh
@@ -5,17 +5,28 @@ realpath() {
 	python -c "import os,sys; print os.path.realpath(sys.argv[1])" "$1"
 }
 
+link() {
+  source="$DOTFILES_PATH/$1" 
+  test -z "$2" && target=~/.$1 || target="$2"
+  unlink "$target"
+  ln -sf "$source" "$target"
+}
+
 BASE_DIR=$(dirname "$0" && cd .. && pwd)
 RESOURCE_DIR="$BASE_DIR/scripts/resources/"
 DOTFILES_PATH=$(realpath "$SCRIPT_DIR/../dotfiles")
 
-rm ~/.vim ~/.vimrc ~/.nvim ~/.nvimrc ~/.slate ~/Library/Application\ Support/Karabiner/private.xml
-ln -sf "$DOTFILES_PATH/vimrc" ~/.vimrc
-ln -sf "$DOTFILES_PATH/vim" ~/.vim
+link vim ~/.config/nvim
+link zshrc
 
-ln -sf "$DOTFILES_PATH/vimrc" ~/.nvimrc
-ln -sf "$DOTFILES_PATH/vim" ~/.nvim
+# rm ~/.vim ~/.vimrc ~/.nvim ~/.nvimrc ~/.slate ~/Library/Application\ Support/Karabiner/private.xml
+# ln -sf "$DOTFILES_PATH/vimrc" ~/.vimrc
+# ln -sf "$DOTFILES_PATH/vim" ~/.vim
 
-ln -sf "$DOTFILES_PATH/slate/slate" ~/.slate
+# ln -sf "$DOTFILES_PATH/vimrc" ~/.nvimrc
+# ln -sf "$DOTFILES_PATH/vim" ~/.nvim
 
-ln -sf "$DOTFILES_PATH/karabiner/private.xml" ~/Library/Application\ Support/Karabiner/private.xml
+test $(uname) = "Darwin" && {
+  link slate/slate ~/.slate
+  link karabiner/private.xml ~/Library/Application\ Support/Karabiner/private.xml
+}
