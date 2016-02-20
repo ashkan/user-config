a93f32e9681e6df242b6853190c14e68612a5946
Ashkan Kiani <ashkan.k.kiani@gmail.com>
2016-02-19 20:20:13 -0800

    Adds mjolnir configuration and readme.

---
+++ dotfiles/mjolnir/README.md
@@ -0,0 +1,61 @@
+# Mjolnir configuration
+
+All my keys are prefixed with `hyper` which is `ctrl+shift+alt+cmd`, which is bound to a single key on my keyboard. This is set to a variable `hyper` and can be easily changed.
+
+## Hotkeys
+
+### Grid system
+
+#### Mapping a key to a cell (section of the screen)
+The thought process behind this is ease of moving and resizing windows on a grid
+system capable of being done solely by my left hand.
+
+Considering a standard layout of QWERTY, the left hand on my keyboard makes a
+3x5 grid with [QWERTY, ASDFG, ZXCVB] being the rows. Each character is mapped to
+a cell in this grid.
+
+|Q|W|E|R|T|
+|:---: |:---: |:---: |:---: |:---: |
+| **A** | **S** | **D** | **F** | **G** |
+| **Z** | **X** | **C** | **V** | **B** |
+
+#### Spanning multiple cells
+Combining 2 or more of these keys will use the rectangle defined by the keys to
+create a layout spanning those corners on the outermost edges. For example,
+pressing `hyper+Q+A` will resize the window to span a 2 cell tall window in the
+top left of the screen. Another example, pressing `hyper+Q+A+S+D` if you have
+that many fingers would produce a 2 tall and 3 wide window in the top left
+corner of the screen. Redundant keys are ignored.
+
+#### Configuration
+
+The variable `grid_keys`, `grid.GRIDWIDTH`, and `grid.GRIDHEIGHT` determine the
+configuration for this system.
+
+### Spaces
+
+- `hyper + ]`: Switch to next space
+- `hyper + [`: Switch to previous space
+- `hyper + =`: Move focused window to next space
+- `hyper + -`: Move focused window to previous space
+
+### Focus
+
+- `hyper + u`: Focus or open iTerm2
+- `hyper + y`: Focus or open Emacs (I use Spacemacs, vim 4 lyfe)
+- `hyper + o`: Focus or open Google Chrome
+- `hyper + \`: Focus or open Dash
+- `hyper + right`: Focus window to the right
+- `hyper + left`: Focus window to the left
+- `hyper + up`: Focus window above
+- `hyper + down`: Focus window below
+
+### Hints
+- `hyper + h`: Show window hints for all windows
+- `hyper + n`: Show window hints for current application windows
+
+### Layouts
+- `hyper + l`: Cycle layouts
+- `hyper + j`: Next window in layout
+- `hyper + k`: Previous window in layout
+- `hyper + return`: Promote window in layout to main window
---
+++ dotfiles/mjolnir/init.lua
@@ -0,0 +1,159 @@
+---------- IMPORTS -------------------------------------------------------------
+local application = require "mjolnir.application"
+local hotkey = require "mjolnir.hotkey"
+local window = require "mjolnir.window"
+local fnutils = require "mjolnir.fnutils"
+local tiling = require "mjolnir.tiling"
+local hints = require "mjolnir.th.hints"
+local grid = require "mjolnir.bg.grid"
+
+---------- HELPERS -------------------------------------------------------------
+function shallowCopy(original)
+    local copy = {}
+    for key, value in pairs(original) do
+        copy[key] = value
+    end
+    return copy
+end
+
+local event = require "mjolnir._asm.eventtap.event"
+
+function postKey(mods, key, isdown)
+  event.post(
+    event.newkeyevent(mods, key, isdown)
+  )
+end
+function mouseClick(point, button, type)
+  event.post(
+    event.newmouseevent(event.types[button.."mouse"..type], point, button)
+  )
+end
+
+
+function chars(s)
+  local t = {}
+  s:gsub(".", function(c) table.insert(t, c) end)
+  return t
+end
+
+---------- CONFIG --------------------------------------------------------------
+local hyper = {"cmd", "alt", "ctrl", "shift"}
+grid.MARGINX = 0
+grid.MARGINY = 0
+grid.GRIDWIDTH = 5
+grid.GRIDHEIGHT = 3
+local grid_keys = chars("qwertasdfgzxcvb")
+
+---------- RELOAD --------------------------------------------------------------
+hotkey.bind(hyper, "i", mjolnir.reload)
+
+---------- SPACES --------------------------------------------------------------
+local mouse = require "mjolnir._asm.sys.mouse"
+local spaces = require("mjolnir._asm.hydra.undocumented").spaces
+function dragToSpace(n)
+  local saved = mouse.get()
+  local focused = window.focusedwindow()
+  local newpos = focused:topleft()
+  newpos.x = newpos.x + math.floor(focused:size().w / 2)
+  newpos.y = newpos.y + 5
+  mouseClick(newpos, "left", "down")
+  spaces.movetospace(spaces.currentspace() + n)
+  mouseClick(newpos, "left", "up")
+  os.execute("sleep 0.01")
+  mouse.set(saved)
+end
+
+hotkey.bind(hyper, "]", function() spaces.movetospace(spaces.currentspace()+1) end)
+hotkey.bind(hyper, "[", function() spaces.movetospace(spaces.currentspace()-1) end)
+hotkey.bind(hyper, "=", function() dragToSpace(1) end)
+hotkey.bind(hyper, "-", function() dragToSpace(-1) end)
+
+---------- HINTS ---------------------------------------------------------------
+local hinting = false
+function toggleHints()
+  if hinting then
+    hints.closeAll()
+  else
+    hints.windowHints()
+  end
+  hinting = not hinting
+end
+
+hotkey.bind(hyper, "h", hints.windowHints)
+-- hotkey.bind(hyper, "h", toggleHints)
+
+local appfinder = require "mjolnir.cmsj.appfinder"
+
+hotkey.bind(hyper, "n", function() hints.appHints(window.focusedwindow():application()) end)
+
+---------- FOCUS ARROWS/APPS ---------------------------------------------------
+hotkey.bind(hyper, "y", function() application.launchorfocus("Emacs") end)
+hotkey.bind(hyper, "u", function() application.launchorfocus("iTerm") end)
+hotkey.bind(hyper, "\\", function() application.launchorfocus("Dash") end)
+hotkey.bind(hyper, "o", function() application.launchorfocus("Google Chrome") end)
+
+hotkey.bind(hyper, "left", function() window.focusedwindow():focuswindow_west() end)
+hotkey.bind(hyper, "right", function() window.focusedwindow():focuswindow_east() end)
+hotkey.bind(hyper, "up", function() window.focusedwindow():focuswindow_north() end)
+hotkey.bind(hyper, "down", function() window.focusedwindow():focuswindow_south() end)
+
+
+---------- TILING --------------------------------------------------------------
+hotkey.bind(hyper, "l", tiling.cyclelayout)
+hotkey.bind(hyper, "j", function() tiling.cycle(1) end)
+hotkey.bind(hyper, "k", function() tiling.cycle(-1) end)
+hotkey.bind(hyper, "return", tiling.promote)
+
+tiling.set("layouts", {
+             "fullscreen", "main-vertical"
+})
+
+---------- GRID ----------------------------------------------------------------
+function make_grid_lookup(keys, w)
+  local lookup = {}
+  for index, key in pairs(keys) do
+    index = index - 1
+    lookup[key] = {x = index % w, y = math.floor(index / w)}
+  end
+  return lookup
+end
+
+
+function grid.set_current(x, y, w, h)
+    local current_window = window.focusedwindow()
+    grid.set(
+      current_window,
+      {x=x, y=y, w=w, h=h},
+      current_window:screen()
+    )
+end
+
+
+local grid_state = {}
+local grid_lookup = make_grid_lookup(grid_keys, grid.GRIDWIDTH)
+
+for i, key in ipairs(grid_keys) do
+  grid_state[key] = false
+  
+  local pressed = function()
+    grid_state[key] = true
+    local min = shallowCopy(grid_lookup[key])
+    local max = shallowCopy(grid_lookup[key])
+    for okey, state in pairs(grid_state) do
+      if state then
+        local pos = grid_lookup[okey]
+        -- if pos.x <= min.x and pos.y <= min.y then min = pos end
+        -- if pos.x >= max.x and pos.y >= max.y then max = pos end
+        min.x = math.min(min.x, pos.x); min.y = math.min(min.y, pos.y)
+        max.x = math.max(max.x, pos.x); max.y = math.max(max.y, pos.y)
+      end
+    end
+    grid.set_current(min.x, min.y, max.x - min.x + 1, max.y - min.y + 1)
+  end
+  
+  local released = function()
+    grid_state[key] = false
+  end
+  
+  hotkey.bind(hyper, key, pressed, released)
+end
